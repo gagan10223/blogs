8cfc6d2bfc74924d6eae712a6e655e88370ce3d0
System <gagan10223@gmail.com>
2023-10-03 14:46:20 -0400

    all

--- blog.js
+++ blog.js
@@ -1,16 +1,54 @@
 const express = require('express');
+const morgan = require('morgan');
+const mon = require('mongoose');
+const Blog = require('./schema');
+
+const dburi = 'mongodb+srv://gagan:1234@cluster0.oljierj.mongodb.net/node?retryWrites=true&w=majority'
 
 const app = express();
 
-app.listen(3000);
+mon.connect(dburi,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(result => app.listen(3000))
+.catch((err)=> console.log(err))
 
-app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
-})
+
+app.use(express.static('public'));
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', (req, res) => {
+    Blog.find()
+        .then((blogs) => {
+            res.render('index', { blogs });
+        })
+        .catch((error) => {
+            res.status(500).send('An error occurred: ' + error.message);
+        });
+});
+
+
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{root:__dirname});
+    res.render('about');
+})
+
+app.get('/create',(req,res)=>{
+    res.render('create');
+})
+
+app.post('/create',(req,res)=>{
+    const blog = new Blog(req.body);
+    blog.save()
+    
+    .then(result => {console.log(result)
+          res.redirect('/')})
+    .catch(err => {console.log(err)})
 })
 
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname})
+    res.status(404).render('index');
 })
